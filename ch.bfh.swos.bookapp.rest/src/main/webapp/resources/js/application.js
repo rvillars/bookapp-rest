0b0eb0ff3968076be5ace37d2b1c8c1560e2b237
Roger Villars <roger.villars@mimacom.com>
2013-12-13 09:03:26 +0100

    Reformat and organize imports

--- ch.bfh.swos.bookapp.rest/src/main/webapp/resources/js/application.js
+++ ch.bfh.swos.bookapp.rest/src/main/webapp/resources/js/application.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var bookapp = angular.module('bookapp', ['controllers', 'services','ui.bootstrap']);
+var bookapp = angular.module('bookapp', ['controllers', 'services', 'ui.bootstrap']);
 
 bookapp.config(function ($routeProvider) {
     $routeProvider.
