0ff2940e3c3dff9973223d62b1ea9a371117c28e
Roger Villars <roger.villars@bluewin.ch>
2015-05-06 13:40:35 +0200

    added db init script

---
+++ ch.bfh.swos.bookapp.domain/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into author(id, firstname, lastname) values (1, 'J.R.R', 'Tolkien');
+insert into author(id, firstname, lastname) values (2, 'Stephen', 'King');
+
+insert into book(id, title, releasedate, author_id) values (1, 'Lord of the Rings - Fellowship of the Ring', '2013-04-01', 1);
+insert into book(id, title, releasedate, author_id) values (2, 'Lord of the Rings - Return of the King', '2011-02-15', 1);
